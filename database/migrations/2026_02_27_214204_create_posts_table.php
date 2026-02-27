7695239ffbac4d31bcde8582b1fbac3938bc4d99
kosukeurushima <urushima.kosuke@gmail.com>
2026-02-27 22:03:38 +0000

    テーブル作成

--- database/migrations/2026_02_27_214204_create_posts_table.php
+++ database/migrations/2026_02_27_214204_create_posts_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('bio')->nullable();
-            $table->string('image')->nullable()->default('dawn.png');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->string('post', 400);
             $table->timestamps();
+
+            $table->foreign('user_id')
+              ->references('id')
+              ->on('users')
+              ->onDelete('cascade');
         });
     }
 
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
